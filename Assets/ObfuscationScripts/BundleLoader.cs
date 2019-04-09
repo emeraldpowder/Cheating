409ae5f88954866f3c2129df04c0ea4b192bb8e7
Glavak <gau-gau@ya.ru>
2019-04-09 14:55:08 +0700

    Added bundle encrypter

---
+++ Assets/ObfuscationScripts/BundleLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BundleLoader : MonoBehaviour
+{
+    public const byte salt = 89;
+    
+    private void Start()
+    {
+        LoadDecrypted("mybundle");
+
+        var assetBundle = LoadDecrypted("scenebundle");
+        string[] scenePath = assetBundle.GetAllScenePaths();
+        SceneManager.LoadScene(scenePath[0]);
+    }
+
+    private AssetBundle LoadDecrypted(string name)
+    {
+        var bytes = File.ReadAllBytes(@"D:\Documents\YouTube\Cheating\Assets\AssetBundles\" + name);
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            bytes[i] = (byte) (bytes[i] ^ salt);
+        }
+
+        var bundle = AssetBundle.LoadFromMemory(bytes);
+        if(!bundle.isStreamedSceneAssetBundle) bundle.LoadAllAssets();
+        return bundle;
+    }
+}
---
+++ Assets/ObfuscationScripts/Editor/CreateAssetBundles.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+using File = UnityEngine.Windows.File;
+
+public class CreateAssetBundles : MonoBehaviour
+{
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None,
+            BuildTarget.StandaloneWindows);
+
+        // Encrypt them:
+        foreach (string file in Directory.GetFiles(assetBundleDirectory))
+        {
+            if (file.EndsWith(".meta") || file.EndsWith(".manifest")) continue;
+
+            var bytes = File.ReadAllBytes(file);
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = (byte) (bytes[i] ^ BundleLoader.salt);
+            }
+
+            File.WriteAllBytes(file, bytes);
+        }
+    }
+}
